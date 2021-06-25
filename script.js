da7174c1add833bf8eb95313f8f3c1602c3a490b
VictoriaAde <adedayovicky123@gmail.com>
2021-06-25 02:20:59 +0100

    playsound on click - incomplete

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; //stop the function from running 
-    audio.currentTime = 0; //rewind  to the start
-    audio.play();
-    key.classList.add('playing');
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stop the function from running
+  audio.currentTime = 0; //rewind  to the start
+  audio.play();
+  key.classList.add("playing");
 }
 function transitionend(e) {
-    if (e.propertyName !== 'transform') return; //skip if its not a transform 
-    this.classList.remove('playing');
+  if (e.propertyName !== "transform") return; //skip if its not a transform
+  this.classList.remove("playing");
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', transitionend));  //listen for a transitionend evt on each of the key
 
-window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", transitionend)); //listen for a transitionend evt on each of the key
+
+window.addEventListener("keydown", playSound);
+
+/////////////////aplaying sound onclick
+keys.forEach((key) =>
+  key.addEventListener("click", function playSoundOnClick(e) {
+    const audios = document.querySelectorAll("audio");
+    audios.forEach((audio) => audio.play());
+  })
+);
+
+
+
+
+
+
+
+
+// ['keydown', 'click'].forEach(e => {
+//     window.addEventListener(e, playSound)
+// })
+
+// window.addEventListener('click', function playSoundOnClick(e){
+//     const audios = document.querySelectorAll('audio');
+//     audios.forEach(audio => audio.play())
+//     //console.log(audios);
+
+// })
+
+// keys.forEach(key => key.addEventListener('click', function(e){
+//     const audio = document.querySelectorAll('audio')
+//     console.log(audio);
+// }))
+
+// ['keydown', 'click'].forEach(e => {
+//     window.addEventListener(e, playSound)
+// })
