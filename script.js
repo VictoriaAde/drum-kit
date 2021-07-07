d03577b2412e7e8025a20a9b0a5c020f26708a0b
VictoriaAde <adedayovicky123@gmail.com>
2021-07-07 21:57:16 +0100

    yay completed

--- script.js
+++ script.js
@@ -6,56 +6,23 @@ function playSound(e) {
   audio.play();
   key.classList.add("playing");
 }
+window.addEventListener("keydown", playSound);
+
+
 function transitionend(e) {
   if (e.propertyName !== "transform") return; //skip if its not a transform
   this.classList.remove("playing");
 }
-
 const keys = document.querySelectorAll(".key");
-
 keys.forEach((key) => key.addEventListener("transitionend", transitionend)); //listen for a transitionend evt on each of the key
 
-window.addEventListener("keydown", playSound);
 
-/////////////////aplaying sound onclick
+/////////////////play sound onclick
 keys.forEach((key) => {
-  key.addEventListener("click", function playSoundOnClick(e) {
-    console.log(this.classList);
-    //e.preventDefault();
-
-    // const audioQ = document.querySelector("audio.q");
-    // audioQ.play()
-
-    // const audioW = document.querySelector("audio.w");
-    //       audioW.play()
-
-    const audioQ = document.querySelector("audio.q");
-    audioQ.play();
-
-   // const key = document.querySelector(".key");
-    //console.log(key);
-
-    // if(audioW){
-    // }
-
-    // const audioE = document.querySelector("audio.e");
-    // audioE.play();
+  key.addEventListener("click", function (e) {
+    const id = key.id;
+    const audio = document.querySelector(`audio#${id}`);
+    audio.play();
+    key.classList.add("playing");
   });
 });
-
-//Plays all audio on a click
-// keys.forEach((key) => {
-//   key.addEventListener("click", function playSoundOnClick(e) {
-//     //e.preventDefault();
-//     const audios = document.querySelectorAll("audio");
-//     audios.forEach((audio) => {
-//       audio.play();
-//       console.log(audio);
-//     });
-//     console.log(audios, "do re mi");
-//   });
-// });
-
-// ['keydown', 'click'].forEach(e => {
-//     keys.addEventListener(e, playSound)
-// })
